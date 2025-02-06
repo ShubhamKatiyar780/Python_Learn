ed4b1dc0e8ba2c6872274071d05fb2ce250425b3
SHUBHAM KATIYAR <168412256+ShubhamKatiyar780@users.noreply.github.com>
2025-02-07 00:03:16 +0530

    Add files via upload

---
+++ SwapTwoNumbersWithoutaTemporaryVariable.py
@@ -0,0 +1,9 @@
+a =int(input("Enter first number: "))
+b =int(input("Enter first number: "))
+print(f"a = {a} and b = {b}")
+
+a = a+b
+b = a-b
+a = a-b
+print("After Swaping of Numbers")
+print(f"a = {a} and b = {b}")
